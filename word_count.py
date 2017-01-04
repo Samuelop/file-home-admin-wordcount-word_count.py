e5deb661a3fced49ca5afc2c8dc502ff46fe458b
Samuel Olivares <samuel.olivares@alumnos.usm.cl>
2017-01-04 11:35:32 -0300

    Minor language change

--- word_count.py
+++ word_count.py
@@ -1,4 +1,4 @@
-happy = input("Enter a statement to word count: ")
+happy = input("Enter a phrase to word count: ")
 
 words = happy.split()
 
@@ -6,5 +6,5 @@ counts = {}
 for word in words:
     counts[word] = counts.get(word, 0) + 1
 
-print("The word frequency of your input is: ")    
+print("The word frequency of your phrase is: ")    
 print(counts)
